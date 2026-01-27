45e40947f294da69d7cc184aa042636428dfa3c1
Akash-Karma <142605289+Akash-Karma@users.noreply.github.com>
2026-01-28 03:27:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (78.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,0);
+       
         int prev=nums[0];
         int prev1=0;
         for(int i=1;i<n;i++){
