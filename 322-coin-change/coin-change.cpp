78f79e7f1eb676e525b27295782743a361d74019
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-03-31 21:42:00 +0530

    Time: 39 ms (45.32%) | Memory: 49 MB (35.36%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -21,8 +21,23 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         if(amount==0) return 0;
         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int cnt= solve(coins,amount,0,dp);
-        if(cnt>=1e7) return -1;
-        return cnt;
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)
+            dp[0][i]=i/coins[0];
+            else dp[0][i]=1e7;
+        }
+        for(int i=1;i<coins.size();i++){
+            for(int j=0;j<=amount;j++){
+                int notPick=0+dp[i-1][j];
+                int pick=1e7;
+                if(j>=coins[i]){
+                    pick=1+dp[i][j-coins[i]];
+                }
+                dp[i][j]=min(pick,notPick);
+            }
+            
+        }
+        if(dp[coins.size()-1][amount]>=1e7) return -1;
+        else return dp[coins.size()-1][amount];
     }
 };
