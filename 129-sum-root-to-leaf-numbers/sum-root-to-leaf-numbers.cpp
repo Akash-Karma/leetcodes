48f2568e372bd098cd6b70439e2cfcb8b43616a1
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-03-31 11:31:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (21.02%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -22,14 +22,14 @@ public:
             if(node->left==NULL && node->right==NULL){
                 sum+=currNum*10+node->val;
             }
-            else{
-                if(node->right !=NULL){
-                    q.push({node->right, currNum*10+node->val});
-                }
-                if(node->left!=NULL){
-                    q.push({node->left, currNum*10+node->val});
-                }
+
+            if(node->right !=NULL){
+                q.push({node->right, currNum*10+node->val});
             }
+            if(node->left!=NULL){
+                q.push({node->left, currNum*10+node->val});
+            }
+
         }
         return sum;
     }
