ae22150c7c49a674eeba19602bd4fdf7e0897cc5
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-04-17 22:30:14 +0530

    Time: 47 ms (18.76%) | Memory: 64.3 MB (14.50%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> ans;
         unordered_map<int, bool> mp;
         for(int i:nums){
-            if(mp.find(i)==mp.end()){
-                mp[i]=1;
-            }
+            
+            mp[i]=1;
+            
         }
         for(int i=1;i<=nums.size();i++){
             if(mp.find(i)==mp.end()){
