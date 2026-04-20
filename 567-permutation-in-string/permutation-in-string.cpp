27b4a5159cad31983add2d3f9eb602747f938ca9
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-04-21 05:27:30 +0530

    Time: 3 ms (65.21%) | Memory: 10.7 MB (23.00%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+
+        unordered_map<char,int> mp1, mp2;
+
+        for(char c : s1) mp1[c]++;
+
+        int k = s1.size();
+
+        for(int i = 0; i < s2.size(); i++){
+            mp2[s2[i]]++;
+
+            if(i >= k){
+                mp2[s2[i - k]]--;
+                if(mp2[s2[i - k]] == 0)
+                    mp2.erase(s2[i - k]);
+            }
+
+            if(mp1 == mp2) return true;
+        }
+
+        return false;
+    }
+};
