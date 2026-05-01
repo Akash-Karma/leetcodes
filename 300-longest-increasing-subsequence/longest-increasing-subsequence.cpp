06637cc89fac622c775d38945ce2e7347a3a872a
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-05-01 11:52:18 +0530

    Time: 527 ms (18.36%) | Memory: 296.7 MB (5.52%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,16 +1,18 @@
 class Solution {
+private:
+    int solve(vector<int> &nums, int curr, int prev, vector<vector<int>> &dp){
+        if(curr<0) return 0;
+        if(dp[curr][prev+1]!=-1) return dp[curr][prev+1];
+        int notPick=solve(nums, curr-1, prev, dp);
+        int pick=0;
+        if(prev==-1 || nums[curr]<nums[prev]){
+            pick=1+solve(nums, curr-1, curr, dp);
+        }
+        return dp[curr][prev+1]=max(pick,notPick);
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        int LIS[n];
-        LIS[0]=1;
-        for(int i=1;i<n;i++){
-            int l=0;
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i]) l=max(l,LIS[j]);
-            }
-            LIS[i]=l+1;
-        }
-        return *max_element(LIS,LIS+n);
+        vector<vector<int>> dp(nums.size()+1, vector<int>(nums.size()+1,-1));
+        return solve(nums, nums.size()-1, -1, dp);
     }
 };
