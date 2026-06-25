3890345f15ac51cfe4a1f35f82b759273ded7e01
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-06-25 11:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (18.88%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,27 +1,37 @@
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int low=0, high=nums.size()-1, first=-1, last=-1;
+private:
+    int upperBound(vector<int>& nums, int target){
+        int idx=-1;
+        int low = 0, high=nums.size()-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>=target){
-                first=mid;
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        if(first==-1 || nums[first]!=target) return {-1,-1};
-        low=0, high=nums.size()-1, last=first;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                last=mid;
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                idx=mid;
                 low=mid+1;
             }
-            else if(nums[mid]>target) high=mid-1;
+            else if(nums[mid] > target) high=mid-1;
             else low=mid+1;
-
         }
-        return {first,last};
+        return idx;
+    }
+    int lowerBound(vector<int>& nums, int target){
+        int idx=-1;
+        int low = 0, high=nums.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                idx=mid;
+                high=mid-1;
+            }
+            else if(nums[mid] > target) high=mid-1;
+            else low=mid+1;
+        }
+        return idx;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int upper = upperBound(nums, target);
+        int low = lowerBound(nums, target);
+        return {low, upper};
     }
 };
