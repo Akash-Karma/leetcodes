fb69f456be1f31cb184fea9837d58eb71ad6004e
Akash Karmakar <142605289+Akash-Karma@users.noreply.github.com>
2026-07-07 12:30:24 +0530

    Time: 432 ms (17.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 178-rank-scores/rank-scores.sql
@@ -0,0 +1,10 @@
+SELECT
+    s1.score,
+    COUNT(s2.score) AS `rank`
+FROM Scores s1, (
+    SELECT DISTINCT score
+    FROM Scores
+) s2
+WHERE s2.score >= s1.score
+GROUP BY s1.id
+ORDER BY s1.score DESC;
